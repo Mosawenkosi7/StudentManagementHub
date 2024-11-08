842afcec2757758954ced274c81d1b73141f6937
compscibaddie <601335@student.belgiumcampus.ac.za>
2024-11-09 00:48:29 +0200

    View students function added

--- Form1.cs
+++ Form1.cs
@@ -12,9 +12,36 @@ namespace StudentManagementHub
 {
     public partial class Form1 : Form
     {
+        private DataHandler handler;
+        private List<Student> students;
         public Form1()
         {
             InitializeComponent();
+            handler = new DataHandler();
+            LoadStudents();
+        }
+        private void LoadStudents()//for viewing data
+        {
+
+            students = handler.LoadStudents();
+            dataGridView1.DataSource = students.Select(s => new { s.StudentId, s.FullName, s.Age, s.Course }).ToList();
+            dataGridView1.DefaultCellStyle.ForeColor = Color.Black;
+        }
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if (dataGridView1.SelectedRows.Count > 0)
+            {
+                var row = dataGridView1.SelectedRows[0];
+                StudentIdTxtBox.Text = row.Cells["StudentID"].Value.ToString();
+                NamesTxtBox.Text = row.Cells["Name"].Value.ToString();
+                Age.Text = row.Cells["Age"].Value.ToString();
+                CourseList.Text = row.Cells["Course"].Value.ToString();
+            }
+        }
+
+        private void ViewAll_Click(object sender, EventArgs e)
+        {
+            LoadStudents();
         }
     }
 }
